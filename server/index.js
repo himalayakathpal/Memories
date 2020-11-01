1fbb5c804d879ec67e2a9b995cc39691dbdbef92
Himalaya <himalayakathpal4@gmail.com>
2020-11-01 23:30:09 +0530

    added Route

---
+++ server/index.js
@@ -0,0 +1,19 @@
+import express from 'express';  
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';  //used for post requests
+import cors from 'cors';
+
+import postRoutes from './routes/posts.js';
+const app = express();
+
+app.use('/posts',postRoutes);
+app.use(bodyParser.json({limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://himalaya:kathpal1@cluster0.mg7jr.mongodb.net/<dbname>?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+
+mongoose.connect(CONNECTION_URL,{useNewUrlParser:true, useUnifiedTopology:true}).then(()=> app.listen(PORT,() => console.log(`Server running on port:  ${PORT}`))).catch((error)=>console.log(error.message));
+mongoose.set('useFindAndModify',false);
